4dfd57dc0f7cde1fa170b5fda789ec3ccae61ffb
Sibula <valtteri.asikainen@paivola.fi>
2017-09-12 14:45:31 +0300

    Replaced the code in tg.py with tg_with_telepot.py

--- tg.py
+++ tg.py
@@ -1,72 +1,97 @@
-import json
-import requests
 import time
-import urllib
+import logging
+import asyncio
+import telepot, telepot.aio, telepot.exception
+from telepot.aio.loop import MessageLoop
+from pprint import pprint
+from uuid import uuid4
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 TOKEN = "380540735:AAFhwCOUrjnLF_9F7yhPP1iFme0Lh-ygI8k"
-URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 
-def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf-8")
-    return content
+class Switch(dict):
+    def __missing__(self, key):
+        return self.get("default") or (lambda x: None)
 
 
-def get_json_from_url(url):
-    content = get_url(url)
-    json_content = json.loads(content)
-    return json_content
+class SensolaBot(telepot.aio.Bot):
+    def __init__(self, token, cmds):
+        super().__init__(token)
+        self.cmds = cmds
+
+    @staticmethod
+    def generate_uuid():
+        return uuid4()
+
+    @staticmethod
+    def register_keyboard(sauna_id):
+        return {"inline_keyboard": [[{"text": "yes",
+                                      "callback_data": sauna_id + "yes"}, ],
+                                    [{"text": "maybe",
+                                      "callback_data": sauna_id + "maybe"}, ],
+                                    [{"text": "no",
+                                      "callback_data": sauna_id + "no"}, ],
+                                    ]}
+
+    @asyncio.coroutine
+    def send_message(self, chat_id, content, reply_markup=None,
+                     disable_notification=True, disable_web_page_preview=True):
+        logging.info(f"Sending message:{chat_id, content}")
+        yield from self.sendMessage(
+            chat_id, content, parse_mode="HTML", reply_markup=reply_markup,
+            disable_notification=disable_notification,
+            disable_web_page_preview=disable_web_page_preview)
+
+    @asyncio.coroutine
+    def handle(self, msg):
+        flavor, details = telepot.flance(msg)
+        # TODO: log?
+        if flavor == "chat" and details[0] == "text":
+            logging.info(f"Received message: {msg['text']}")
+            content_type, chat_type, chat_id = details
+            yield from self.cmds[msg['text']](
+                self, type=chat_type, id=chat_id, text=msg['text'])
+        elif flavor == "inline_query":
+            query_id, sender_id, query = details
+            # Handle inline query
+            pass
+        elif flavor == "chosen_inline_result":
+            result_id, sender_id, query = details
+            # Handle chosen inline result
+            pass
+        elif flavor == "callback_query":
+            query_id, sender_id, data = details
+            # Handle callback query
+            pass
+
+    @asyncio.coroutine
+    def test(self, **kwargs):
+        chat_type, chat_id, text = kwargs.values()
+        print("received message:", chat_type, chat_id, text)
+
+    @asyncio.coroutine
+    def show(self, **kwargs):
+        chat_id = kwargs["id"]
+        print("[send table to:]", chat_id)
+
+    @asyncio.coroutine
+    def sauna(self, **kwargs):
+        chat_id = kwargs["id"]
+        print("[send saunas to:]", chat_id)
 
 
-def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
-    if offset:
-        url += f"&offset={offset}"
-    json_content = get_json_from_url(url)
-    return json_content
+commands = Switch({"/show": SensolaBot.show,
+                   "/sauna": SensolaBot.sauna,
+                   "/test": SensolaBot.test, })
 
+sensola_bot = SensolaBot(TOKEN, commands)
 
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
+loop = asyncio.get_event_loop()
+loop.create_task(MessageLoop(
+    sensola_bot, handle=sensola_bot.handle).run_forever())
 
+print("Listening ...")
 
-def echo_all(updates):
-    for update in updates["result"]:
-        try:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            send_message(text, chat)
-        except Exception as e:
-            print(e)
-
-
-def get_last_chat_id_and_text(updates):
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    message = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return message, chat_id
-
-
-def send_message(text, chat_id):
-    text = urllib.parse.quote_plus(text)
-    url = URL + f"sendMessage?text={text}&chat_id={chat_id}"
-    get_url(url)
-
-
-def main():
-    print("Starting tg-bot")
-    last_update_id = None
-    while True:
-        updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
-        time.sleep(0.5)
-
-if __name__ == '__main__':
-    main()
+loop.run_forever()
